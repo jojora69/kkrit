4743a9598b5766c3ea1e7a43fd2fda71e33bec39
jojora69 <79311460+jojora69@users.noreply.github.com>
2021-03-11 20:59:20 +0700

    Add files via upload

---
+++ AddItemWin.Designer.cs
@@ -0,0 +1,144 @@
+﻿
+namespace _41
+{
+    partial class AddItemWin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.nameBox = new System.Windows.Forms.TextBox();
+            this.quantityBox = new System.Windows.Forms.TextBox();
+            this.priceBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Наименование";
+            // 
+            // nameBox
+            // 
+            this.nameBox.Location = new System.Drawing.Point(101, 23);
+            this.nameBox.Multiline = true;
+            this.nameBox.Name = "nameBox";
+            this.nameBox.Size = new System.Drawing.Size(273, 28);
+            this.nameBox.TabIndex = 1;
+            // 
+            // quantityBox
+            // 
+            this.quantityBox.Location = new System.Drawing.Point(101, 57);
+            this.quantityBox.Multiline = true;
+            this.quantityBox.Name = "quantityBox";
+            this.quantityBox.Size = new System.Drawing.Size(273, 29);
+            this.quantityBox.TabIndex = 2;
+            // 
+            // priceBox
+            // 
+            this.priceBox.Location = new System.Drawing.Point(101, 92);
+            this.priceBox.Multiline = true;
+            this.priceBox.Name = "priceBox";
+            this.priceBox.Size = new System.Drawing.Size(273, 30);
+            this.priceBox.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Количество";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 95);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Цена за шт.";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(270, 146);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(113, 41);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Добавить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 146);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(113, 41);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Назад";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // AddItemWin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(395, 199);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.priceBox);
+            this.Controls.Add(this.quantityBox);
+            this.Controls.Add(this.nameBox);
+            this.Controls.Add(this.label1);
+            this.Name = "AddItemWin";
+            this.Text = "AddItemWin";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox nameBox;
+        private System.Windows.Forms.TextBox quantityBox;
+        private System.Windows.Forms.TextBox priceBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
---
+++ AddItemWin.cs
@@ -0,0 +1,47 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _41
+{
+    public partial class AddItemWin : Form
+    {
+        public AddItemWin()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Items itemsForm = new Items();
+            itemsForm.Show();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string query = "insert into items(name, quatity, price) values('" + nameBox.Text + "', '" + quantityBox.Text + "', '" + priceBox.Text + "');";
+            MySqlConnection conn = DBUtils.GetDBConnection();
+            MySqlCommand cmDB = new MySqlCommand(query, conn);
+            cmDB.CommandTimeout = 60;
+            try
+            {
+                conn.Open();
+                MySqlDataReader rd = cmDB.ExecuteReader();
+                conn.Close();
+                this.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
